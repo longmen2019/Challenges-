68c296ee975dbd6cf7620b0bd064621783f1d66b
longmen2019 <51213632+longmen2019@users.noreply.github.com>
2021-03-26 22:58:35 -0400

    Add files via upload

---
+++ Convert Minutes into Seconds.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 26 22:43:57 2021
+
+@author: User
+"""
+
+# Write a function that takes an integer minutes and converts it to seconds.
+def covert(minutes):
+    return (int(minutes)*60)
