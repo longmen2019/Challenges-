d119228df88c3f87beb6ae47fbc9f1f6f34c6e9e
longmen2019 <51213632+longmen2019@users.noreply.github.com>
2021-03-27 23:29:09 -0400

    Add files via upload

---
+++ math pow() and set add() methods.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 27 23:16:51 2021
+
+@author: User
+"""
+
+#import math library
+import math
+#Return the value of 9 raised to the power of 3
+print(math.pow(9,3))
+
+# python set add() Method
+
+fruits = {"Apple" , "Cherry" , "Banana"}
+fruits.add("orange")
+print(fruits)
+
