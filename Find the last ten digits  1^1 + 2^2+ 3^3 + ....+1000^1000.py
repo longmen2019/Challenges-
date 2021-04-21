d7ac37d5e0ac011d9afdabe7abe0289df7c17477
longmen2019 <51213632+longmen2019@users.noreply.github.com>
2021-04-20 22:29:02 -0400

    Add files via upload

---
+++ Find the last ten digits  1^1 + 2^2+ 3^3 + ....+1000^1000.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 20 22:22:43 2021
+
+@author: User
+"""
+# Find the last ten digits of the series, Find the last ten digits  1^1 + 2^2+ 3^3 + ....+1000^1000
+
+L = 1000
+d = 10 ** 10
+#s = sum(pow(n,n,d) for n in range (1, L + 1))
+s = sum(pow(n,n)for n in range (1, L+1))
+
+
+print("Project Euler 48 Solution = %d" % (s%d))
