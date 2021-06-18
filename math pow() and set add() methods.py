ce24a45b5cf668396e5f9a6116569122efe3604a
longmen2019 <51213632+longmen2019@users.noreply.github.com>
2021-06-17 22:40:33 -0400

    Update math pow() and set add() methods.py

--- math pow() and set add() methods.py
+++ math pow() and set add() methods.py
@@ -9,10 +9,11 @@ Created on Sat Mar 27 23:16:51 2021
 import math
 #Return the value of 9 raised to the power of 3
 print(math.pow(9,3))
+#output: 729.0
 
 # python set add() Method
 
 fruits = {"Apple" , "Cherry" , "Banana"}
 fruits.add("orange")
 print(fruits)
-
+#output: {'orange', 'Banana', 'Cherry', 'Apple'}
