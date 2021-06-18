7386e3e02b4978770a3ccfd93fd831ef3db21fc7
longmen2019 <51213632+longmen2019@users.noreply.github.com>
2021-06-17 22:30:54 -0400

    Update formatting output using the format method.py

--- formatting output using the format method.py
+++ formatting output using the format method.py
@@ -11,22 +11,28 @@ Created on Sat Apr 10 23:05:52 2021
 
 print('I love {} for  "{}"'.format('Geeks' , 'Geeks'))
 
+# Output: I Love Geeks for "Geek" 
+
 # Using format() method and refering
 # A position of the object
 
 print('{0} and {1}'.format('Geeks' , 'Portal'))
+# Geeks and Portal
 
 print('{1} and {0}'.format('Geeks', 'Portal'))
+# Portal and Geeks
 
 #The above formatting can also be done by using f-Strings
 #Although, this features work only with python 3.6 or above.
 
 print(f"I love {'Geeks'} for \"{'Geeks'}!\"")
+# I love Geeks for "Geeks!"
 
 # using format () method and refering
 # a position of the object
 
 print(f"{'Geeks'} and {'Portal'}")
+# Geeks and Portal
       
       
     
