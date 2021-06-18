93167bae3e52b229acbad38c146a27174026fe4a
longmen2019 <51213632+longmen2019@users.noreply.github.com>
2021-06-17 22:46:52 -0400

    Update sum() function in Python.py

--- sum() function in Python.py
+++ sum() function in Python.py
@@ -15,13 +15,13 @@ Sum = sum(numbers,10)
 print(Sum)
 
 #python code to demonstrate the exception of sum
-# arr = ["a"]
-# #start parameter is not provided
-# Sum = sum(arr)
-# print(Sum)
-# #start = 10
-# Sum = sum(arr,10)
-# print(Sum)
+arr = ["a"]
+#start parameter is not provided
+Sum = sum(arr)
+print(Sum)
+#start = 10
+Sum = sum(arr,10)
+print(Sum)
 
 #python code to demonstrate he practical application of sum()
 numbers = [1,2,3,4,5,1,4,5]
