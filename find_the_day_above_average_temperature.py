b5c750806ccffec4e06685998b27d3d4ed1eff51
longmen2019 <51213632+longmen2019@users.noreply.github.com>
2021-08-01 17:18:09 -0400

    Add files via upload

---
+++ find_the_day_above_average_temperature.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug  1 16:40:41 2021
+
+@author: men_l
+"""
+
+numDays = int(input("How many day's temperature? "))
+total = 0
+temp = []
+for i in range (numDays):
+     nextDay = int(input("Day" + str(i+1) + "'s high temp: "))
+     temp.append(nextDay)
+     total += temp[i]
+     
+avg = round(total/numDays,2)
+print("\nAverage = " + str(avg))
+
+above = 0
+for i in temp:
+    if i > avg:
+        above += 1
+print(str(above) + "day(s) above average") 
